3643741803cc1f3291547387768a90e0983d485d
Rutuja Kurhade <Rutuja.Kurhade@KLINGERLINBERG.COM>
2022-12-15 17:58:06 +0530

    Modulus added

--- tutorials/Out/Program.cs
+++ tutorials/Out/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 internal class Program
 {
-    static void Out_parameter(int num1, int num2, out int sum, out int multi, out int div)
+    static void Out_parameter(int num1, int num2, out int sum, out int multi, out int div ,out int mod)
     {
         sum = num1 + num2;
         multi = num1 * num2;
         div = num1 / num2;
+        mod = num1 % num2; 
        
     }
     static void Main()
@@ -13,7 +14,8 @@ internal class Program
         int total = 0;
         int product = 0;
         int divide = 0;
-        Out_parameter(23, 33,out total,out product,out divide);
+        int modulus = 0;
+        Out_parameter(23, 33,out total,out product,out divide,out modulus);
         Console.WriteLine("Addition is = {0} \n Multiplication is = {1} \n Division is = {2} " , total,product,divide);
         Console.ReadKey();
     }
