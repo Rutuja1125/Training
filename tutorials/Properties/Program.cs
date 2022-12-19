bdbb35757044ad4c95c67a2fc589927bbda6fc4d
Rutuja Kurhade <Rutuja.Kurhade@KLINGERLINBERG.COM>
2022-12-19 15:35:55 +0530

    Reading values from class in method

---
+++ tutorials/Properties/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Properties
+    {
+        private int CId;
+        public void SetId(int CId)
+        {
+            this.CId = CId;
+            Console.WriteLine( CId <= 0 ? "kindly enter Class Id greater than 0" : "CId is " + CId);
+            //    if (CId <= 0)
+            //{
+            //    Console.WriteLine("kindly enter Class Id greater than 0");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("CId is " + CId);
+            //}
+
+        }
+
+    }
+    internal class Program
+    {
+  
+        public static void  check(int MId)
+        {
+           Console.WriteLine( MId <= 0? "kindly enter Method Id greater than 0": "MId is " + MId);
+            //if(MId <= 0)
+            //{
+            //    Console.WriteLine("kindly enter Method Id greater than 0");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("MId is " + MId);
+            //}
+        }
+            
+        static void Main()
+        {
+            Console.WriteLine("Enter Id = ");
+            int MId =Convert.ToInt16(Console.ReadLine());
+            check(MId);
+
+            Console.WriteLine("Enter Id = ");
+            int CId = Convert.ToInt16(Console.ReadLine());
+            Properties prop = new Properties();
+            prop.SetId(CId);
+            //Console.WriteLine(prop.GetId());
+            Console.ReadKey();
+        }
+    }
+}
--- tutorials/method_overloading/Program.cs
+++ tutorials/method_overloading/Program.cs
@@ -14,9 +14,22 @@ namespace method_overloading
             int Sum = a + b;
             Console.WriteLine("Addition is = " + Sum);
         }
+        public static void Add(int a, int b,int c) //overloading with different no of parameters
+        {
+            int Sum = a + b +c;
+            Console.WriteLine("Addition is = " + Sum);
+        }
+
+        public static void Add(int a, float b) //overloading with different no of parameters
+        {
+            float Sum = a + b ;
+            Console.WriteLine("Addition is = " + Sum);
+        }
         static void Main()
         {
             Add(2, 4);
+            Add(2, 4,6);
+            Add(2, 4.567F);
             Console.ReadKey();
         }
     }
