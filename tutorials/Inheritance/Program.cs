b631027c8b23ca29d97b7be4b48c266c63e44f3b
Rutuja Kurhade <Rutuja.Kurhade@KLINGERLINBERG.COM>
2022-12-19 10:38:03 +0530

    Method hidding using new keyword in child class.

--- tutorials/Inheritance/Program.cs
+++ tutorials/Inheritance/Program.cs
@@ -9,33 +9,55 @@ namespace Inheritance
 {
     public class KHS
     {
-        static string[] SubMiss = { "A", "B", "C" };
-        public int[] RollNo = { 1, 2, 3 };
-        float fees = 15000F;
+        //public string[] names;
+       public string[] names;
+        public string firstname;
+        //static string[] SubMiss = { "A", "B", "C" };
+        //public int[] RollNo = { 1, 2, 3 };
+        //float fees = 15000F;
 
         //instance method
-        public void Common()
-        {
-            for (int subject = 0; subject < SubMiss.Length; subject++)
-            {
-                Console.WriteLine("This School has " + SubMiss[subject] + " Subject");
+        //public void Common()
+        //{
+        //    //for (int subject = 0; subject < SubMiss.Length; subject++)
+        //    //{
+        //    //    Console.WriteLine("This School has " + SubMiss[subject] + " Subject");
 
+        //    //}
+        //    //Console.WriteLine("Basic fees = " + fees);
+        //}
+        public void display(string[] names)
+        {
+            foreach(string nm in names)
+            {
+                Console.WriteLine(nm);
             }
-            Console.WriteLine("Basic fees = " + fees);
         }
 
     }
     public class Class1 : KHS
     {
-        public int Totfees = 0;
-        public void Students1(string[] Names1)
+        //public int Totfees = 0;
+        //public void Students1(string[] Names1)
+        //{
+        //    for (int i = 0; i < RollNo.Length; i++)
+        //    {
+
+        //        Console.WriteLine("{0} ,{1}", RollNo[i], Names1[i]);
+        //    }
+
+        //}
+       
+
+    }
+    public class Class2 : Class1
+    {
+        public new void display(string[] names)
         {
-            for (int i = 0; i < RollNo.Length; i++)
+            for (int i=0;i < names.Length;i++)
             {
-
-                Console.WriteLine("{0} ,{1}", RollNo[i], Names1[i]);
+                Console.WriteLine("Item at index "+i+" = " + names[i]);
             }
-
         }
 
     }
@@ -44,11 +66,17 @@ namespace Inheritance
     {
         static void Main()
         {
-            KHS k = new KHS();
-            k.Common();
-            Class1 stu = new Class1();
-            string[] Names1 = { "john", "Harry", "William" };
-            stu.Students1(Names1);
+            //    KHS k = new KHS();
+            //    k.Common();
+            //    Class1 stu = new Class1();
+            //    string[] Names1 = { "john", "Harry", "William" };
+            //    stu.Students1(Names1);
+            //KHS K = new KHS();
+            string[] names = { "ABc", "DEF" };
+            //K.display(names);
+            //K.display(names);
+            Class2 c2 = new Class2();
+            c2.display(names);
             Console.ReadKey();
         }
 
---
+++ tutorials/method_hiding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_hiding
+{
+    class KHS //Base class
+    {
+        public void Display() //Method hidden
+        {
+            Console.WriteLine("this is hidden base class method");
+        }
+    }
+    class Fourth:KHS  //Derived Class (Child Class)
+    {
+        public new void Display()
+        {
+            Console.WriteLine("this is new  base class method called in child class");
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Fourth f4= new Fourth();
+            f4.Display();
+
+            KHS k = new KHS();
+            k.Display();
+            Console.ReadKey();
+        }
+    }
+}
