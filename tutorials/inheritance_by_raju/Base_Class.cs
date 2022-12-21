4aa67899b139f94c30c3e3ba06fcbf30151a5b5a
Rutuja Kurhade <Rutuja.Kurhade@KLINGERLINBERG.COM>
2022-12-21 14:25:07 +0530

    Passing value to Parent class Parameterized constructor in erived class constructor using base keyword.

---
+++ tutorials/inheritance_by_raju/Base_Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace inheritance_by_raju
+{
+    internal class Base_Class
+    {
+        internal double salary;
+        public Base_Class(int sal) 
+        {
+            this.salary = sal;
+            Console.WriteLine("This is base class having salary = " + this.salary);
+        }
+        //internal void Bunglow()
+        //{
+        //    Console.WriteLine("This is base class bunglow");
+        //}
+        //protected void Factory()
+        //{
+        //    Console.WriteLine("This is base class factory ");
+        //}
+        ////static void Main()
+        ////{
+        ////    Base_Class bc= new Base_Class();
+        ////    //bc.Job(); //cannot be called either public or internal
+        ////}
+    }
+}
---
+++ tutorials/inheritance_by_raju/Derived_ClassA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_by_raju
+{
+    internal class Derived_ClassA : Base_Class
+    {
+        internal int Child_slary;
+        public Derived_ClassA(int Child_salary) : base(3000)
+        {
+            this.Child_slary = Child_salary;
+            Console.WriteLine("Child class salary = "+this.Child_slary);
+        }
+        public void Job()
+        {
+            Console.WriteLine("child class A works in a company");
+        }
+
+        static void Main()
+        {           
+            Derived_ClassA A = new Derived_ClassA(6000);
+            Console.ReadKey();
+        }
+    }
+}
