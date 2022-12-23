95c25b2fdcb69876aefe31047dbfe898c12216ff
Rutuja Kurhade <Rutuja.Kurhade@KLINGERLINBERG.COM>
2022-12-23 10:17:56 +0530

    Get and Set properties

--- tutorials/Get_set_methods/Program.cs
+++ tutorials/Get_set_methods/Program.cs
@@ -1,23 +1,39 @@
-﻿//using System;
-//namespace Get_set_methods
-//{
-//    internal class Program
-//    {
-//         double Radius = 33.00;
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 
-//        public double  ReadValue()
-//        {
-//            return Radius;
-//        }
-//        public void SetValue(double radius)
-//        {
-//            Radius = radius;
-//        }
-//        static void Main(string[] args)
-//        {
-//            Program p= new Program();
-//            p.ReadValue();//Get method-->Readonly method
-//            Console.ReadKey();
-//        }
-//    }
-//}
+namespace Get_set_methods
+{
+    internal class Program
+    {
+        double Radius = 33.00;
+
+        //public double ReadValue()
+        //{
+        //    return Radius;
+        //}
+        //public void SetValue(double radius)
+        //{
+        //    Radius = radius;
+        //}
+        public double RadiusProperty
+        {
+            get
+            { return Radius; }
+            set
+            {
+                Radius = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //double print_radius = p.ReadValue();//Get method-->Readonly method
+            //Console.Write("Readonly value of radius = " + print_radius);
+            //p.SetValue(22);
+            p.RadiusProperty = 55.88;//setting the value-->writing 
+            double print_radius = p.RadiusProperty;//Get method-->Readonly method
+            Console.Write("Readonly value of radius = " + print_radius);
+            Console.ReadKey();
+        }
+    }
+}
