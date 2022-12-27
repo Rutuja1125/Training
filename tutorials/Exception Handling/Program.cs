29973c584c81c4bd28c37e0ad67341c3069907d0
Rutuja Kurhade <Rutuja.Kurhade@KLINGERLINBERG.COM>
2022-12-27 16:13:24 +0530

    try catch and finally block

---
+++ tutorials/Exception Handling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Program
+    {
+        public static void Divide(int x,int y)
+        {
+            try
+            {
+                Console.WriteLine("Division  is = "+(x/y));
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Re-enter inputs ");
+            }
+        }
+       
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("enter int x = ");
+                int x = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("enter int y = ");
+                int y = Convert.ToInt16(Console.ReadLine());
+                Divide(x,y);
+               
+
+            }
+            catch 
+            {
+                Console.WriteLine("enter proper inputs");
+                Console.ReadKey();
+            }
+           
+
+        }
+    }
+}
---
+++ tutorials/indexers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexers
+{
+    internal class Program
+    {
+        private string[] Data=new string[5];
+
+        //indexers
+        public string this[int index] //Accessed using instance of class
+        {
+            get //Readonly access
+            { return Data[index]; }
+            set //Writing values
+            {
+                Data[index] = value;
+            }
+        }
+        static void Main()
+        {
+            Program p= new Program();
+            p[0] = "ABC";
+            p[1] = "DEF";
+            p[2] = "GHI";
+            for (int i=0; i<5;i++)
+            {
+                Console.WriteLine(p[i]);
+            }
+                   Console.ReadKey();
+        }
+    }
+}
