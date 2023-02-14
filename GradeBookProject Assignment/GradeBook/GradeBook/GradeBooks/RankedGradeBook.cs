9e25760c10a6d1dd9a088d3e0ec349bf5b196450
Rutuja Kurhade <Rutuja.Kurhade@KLINGERLINBERG.COM>
2023-02-14 16:25:12 +0530

    GradeBook Project

---
+++ GradeBookProject Assignment/GradeBook/GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,56 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name,bool isWeighted) : base(name, isWeighted)
+        {
+            Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+          
+            if (averageGrade >= 80)
+                return 'A';
+
+            else if (( 80 > averageGrade) &&(averageGrade >= 60))
+                return 'B';
+
+            else if ((60 > averageGrade) && (averageGrade >= 40))
+                return 'C';
+
+
+            else if ((40 > averageGrade) && (averageGrade >= 20))
+                return 'D';
+
+            else
+                return 'F';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading atleast 5 students");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5) 
+            {
+                Console.WriteLine("Ranked grading atleast 5 students");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
+    }
+}
